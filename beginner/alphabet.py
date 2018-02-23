a400e6dc616f83a78837484ccf7341c12c24d2d8
madhan kumar <madhanraj0707@gmail.com>
2018-02-23 14:01:04 +0530

    Create alphabet.py

---
+++ beginner/alphabet.py
@@ -0,0 +1,5 @@
+alphabet=raw_input("Enter the letter to check:")
+if((alphabet>="a" and alphabet<="z") or(alphabet>="A" and alphabet<="Z")):
+	print ("It is an alphabet")
+else:
+	print ("It is not an alphabet")

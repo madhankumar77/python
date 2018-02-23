a3944ad24ce9b38a91384ae46a6273ac4cf99b36
madhan kumar <madhanraj0707@gmail.com>
2018-02-23 14:13:51 +0530

    Create largestnum.py

---
+++ beginner/largestnum.py
@@ -0,0 +1,11 @@
+a=int(raw_input("enter the 1st number:"))
+b=int(raw_input("enter the 2nd number:"))
+c=int(raw_input("enter the 3rd number:"))
+temp0=0
+if(a>b):
+	temp=a
+else:
+	temp=b
+if(temp<c):
+	temp=c
+print temp

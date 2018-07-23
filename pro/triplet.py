05a2254614a8c4de6b7eea765957ef9949f1ba72
madhan kumar <madhanraj0707@gmail.com>
2018-07-23 11:08:58 +0530

    Update triplet.py

--- pro/triplet.py
+++ pro/triplet.py
@@ -1,1 +1,21 @@
+def triplet(tot,l):
+	m=len(l)
+	for i in range(m-2):
+		for j in range(i+1,m-1):
+			for k in range(j+1,m):
+				if l[i]+l[j]+l[k]==tot:
+					return l[i],l[j],l[k]
 
+	return 'none'
+  
+def main():
+	m=int(input())
+	l=[]
+	for i in range(m):
+		l.append(int(input()))
+	tot=int(input())
+	print(triplet(tot,l))
+try:
+  main()
+except:
+  print('invalid')

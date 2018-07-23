9b884234e3078e621f41c4ea22661328dbf224fb
madhan kumar <madhanraj0707@gmail.com>
2018-07-23 10:01:29 +0530

    Update roman.py

--- Player/roman.py
+++ Player/roman.py
@@ -1,42 +1,20 @@
-try:
-	rom=str(raw_input())
-	i=0
-	k=0
-	a=[]
-	while(i<len(rom)):
-		if(rom[i]=='I'):
-			a.append(1)
-		elif(rom[i]=='X'):
-			a.append(10)
-		elif(rom[i]=='L'):
-			a.append(50)
-		elif(rom[i]=='C'):
-			a.append(100)
-		elif(rom[i]=='D'):
-			a.append(500)
-		elif(rom[i]=='M'):
-			a.append(1000)
-		else:
-			a.append(0)
-			break
-		i=i+1
-	j=len(a)-1
-	i=len(a)-1
-	k=a[j]
-	while(i>0):
-		if(a[i]!=0):
-			while(j>0):
-				if(a[j]==a[j-1] or a[j]<a[j-1]):
-					k=k+a[j-1]
-				elif(a[j]>a[j-1]):
-					k=k-a[j-1]
-				else:
-					pass
-				j=j-1
-			print k	
-		else:
-			print "Invalid"
-			break
-		i=i-1	
-except:
-	print "Invalid"
+def romanMap():
+    map=(("M",  1000),("CM", 900),("D",  500),("CD", 400),("C",  100),("XC", 90),("L",  50),("XL", 40),("X",  10),("IX", 9),("V",  5),("V", 4),("I",  1))
+    return map
+firstNum=ns([0])
+secondNum=ns([1])
+def main():
+    ns=str(input("Enter a roman numeral"))
+    total=0
+    result=0
+    while ns:
+        firstNum=(romanMap(ns[0]))
+        secondNum=(romanMap(ns[1]) 
+        if firstNum is len(ns)>1 and secondNum-1:
+                        total=total+firstNum
+            ns=ns[1:]
+        else:
+                        total=total+ns[1]-ns[0]
+            ns=ns[2:]
+      print (total)
+main()
